119ac505238ecc93bd08f185defb3188134b0f8f
munadi1406 <munadifathullah123@gmail.com>
2023-10-07 21:11:32 +0800

    fix: error index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./layouts/dashboard";
-import Login from './layouts/auth';
+import Login from './pages/auth/Login'
 
 function App() {
   return (
     <div className="font-Nunito">
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route element={<Login />} index/>
           <Route path="/dashboard/*" element={<Dashboard />} />
         </Routes>
     </div>

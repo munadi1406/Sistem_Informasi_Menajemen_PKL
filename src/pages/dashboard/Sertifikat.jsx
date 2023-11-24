b06749ed36af5e9e76e02a472c03cbe61017ea2d
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-11-24 13:22:43 +0000

    feat: fix bug certificate generate

--- src/pages/dashboard/Sertifikat.jsx
+++ src/pages/dashboard/Sertifikat.jsx
@@ -2,13 +2,12 @@ import { useQuery } from "react-query";
 import Loader from "../../components/Loader";
 import Select from "react-select";
 import makeAnimated from "react-select/animated";
-import { useEffect, useState,  useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getListTemplateSertifikat } from "../../api/templateSertifkat";
 import { endpoint } from "../../api/users";
 import TextInput from "../../components/TextInput";
 import TextAreaCustom from "../../components/TextAreaCustom";
 import { getDetailKepsek } from "../../api/kepsek";
-
 const animatedComponents = makeAnimated();
 import QrCode from "../../components/QrCode";
 import { Checkbox, Slider } from "@material-tailwind/react";
@@ -16,7 +15,8 @@ import ButtonCustom from "../../components/ButtonCustom";
 import { useReactToPrint } from "react-to-print";
 import LazyImage from "../../components/LazyImage";
 import Draggable from "react-draggable";
-
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 export default function KartuPelajar() {
   const [valueSearch, setValueSearch] = useState("");
@@ -26,8 +26,9 @@ export default function KartuPelajar() {
   const [leadEvent, setLeadEvent] = useState("");
   const [isQrCode, setIsQrCode] = useState(false);
   const [qrCodeSize, setQrCodeSize] = useState(30);
-
-  const targetRef = useRef();
+  const [nomorSertifikat, setNomorSertifikat] = useState();
+  const [typeSertifikat, setTypeSertifikat] = useState("PENGHARGAAN");
+  const [loading, setLoading] = useState(false);
 
   const { isLoading, data, refetch, isRefetching } = useQuery(
     `listSertifikatGenerate`,
@@ -69,26 +70,15 @@ export default function KartuPelajar() {
   };
 
   useEffect(() => {
-    console.log(splitName);
-  }, [splitName]);
-
-  useEffect(() => {
     refetch();
   }, [valueSearch]);
   const ref = useRef();
 
-  const handlePrint = useReactToPrint({
-    content: () => ref.current,
-    pageStyle: `
-      @page {
-        size: landscape;
-      }
-    `,
-  });
-
   const [imageBlob, setImageBlob] = useState(null);
 
   useEffect(() => {
+    if (!value.template) return;
+    console.log("running");
     const imageUrl = `${endpoint}/templateSertifikat/image/${value.template}`;
 
     const fetchImage = async () => {
@@ -105,10 +95,58 @@ export default function KartuPelajar() {
 
     fetchImage();
   }, [value]);
+
   const [defaultPosition, setDefaultPosition] = useState({ x: 0, y: 0 });
-  const [defaultPositionKepsek, setDefaultPositionKepsek] = useState({ x: 0, y: 0 });
-  const [defaultPositionKepel, setDefaultPositionKepel] = useState({ x: 0, y: 0 });
-  const [defaultPositionQrCode, setDefaultPositionQrCode] = useState({ x: 0, y: 0 });
+  const [defaultPositionKepsek, setDefaultPositionKepsek] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [defaultPositionKepel, setDefaultPositionKepel] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [defaultPositionQrCode, setDefaultPositionQrCode] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [defaultPositionNomor, setDefaultPositionNomor] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  const generatePDF = async () => {
+    setLoading(!loading);
+    const pdf = new jsPDF("l", "mm", "a4"); // 'p' for portrait, 'mm' for millimeters
+    const pages = document.querySelectorAll(".certificate-page");
+
+    const promises = Array.from(pages).map(async (page, i) => {
+      const canvas = await html2canvas(page, { scale: 4 });
+      const imageData = canvas.toDataURL("image/jpeg");
+      return { index: i, data: imageData };
+    });
+
+    const screenshots = await Promise.all(promises);
+
+    screenshots.forEach((screenshot, i) => {
+      if (i > 0) {
+        pdf.addPage();
+      }
+      pdf.addImage(
+        screenshot.data,
+        "JPEG",
+        0,
+        0,
+        pdf.internal.pageSize.width,
+        pdf.internal.pageSize.height,
+      );
+    });
+    pdf.autoPrint();
+    pdf.save(
+      `${splitName.join("-")}-${perihal}-${new Date().toLocaleString()}.pdf`,
+    );
+    setLoading(false);
+  };
+
   if (isLoading) {
     return (
       <>
@@ -127,6 +165,10 @@ export default function KartuPelajar() {
       <div className="w-full flex justify-end">
         <ButtonCustom text={"History Pembuatan Sertifikat"} />
       </div>
+      <TextInput
+        label={"Nomor Sertifikat"}
+        onChange={(e) => setNomorSertifikat(e.target.value)}
+      />
       <Select
         options={options}
         isLoading={isRefetching}
@@ -162,67 +204,116 @@ export default function KartuPelajar() {
         />
       )}
 
-      <div
-        className="certificate w-full space-y-2 h-max overflow-auto "
-        ref={ref}
-      >
-        {splitName.map((e, i) => (
-          <div className="relative flex justify-start w-[842px] h-[595px] t" key={i}> 
-            <>
+      <div className="certificate w-full h-max overflow-auto " ref={ref}>
+        {value.template &&
+          splitName.map((e, i) => (
+            <div className="relative  w-full certificate-page" key={i}>
               <LazyImage src={URL.createObjectURL(imageBlob)} alt={"image"} />
-              <Draggable position={defaultPosition} onStop={(e, data) => setDefaultPosition({ x: data.x, y: data.y })}>
-                <div className="absolute testing">
-                <div className="w-full">
-                  <p className="text-4xl font-semibold text-center font-serif">
-                    {" "}
-                    CERTIFICATE{" "}
-                  </p>
-                  <p className="text-semibold text-center  uppercase ">
-                    {" "}
-                    Penghargaan{" "}
-                  </p>
-                  <p className="text-semibold text-center  text-lg font-semibold m-4">
-                    Diberikan Kepada :
-                  </p>
-                </div>
-                <div className="">
-                  <p className="text-3xl font-semibold text-center font-serif mb-5">
-                    {e}
-                  </p>
-                  <p className="text-md font-semibold text-center font-serif w-[300px]  break-words">
-                    {perihal}
-                  </p>
-                </div>
+              <Draggable
+                bounds="parent"
+                position={defaultPosition}
+                onStop={(e, data) =>
+                  setDefaultPosition({ x: data.x, y: data.y })
+                }
+              >
+                <div
+                  className={`absolute top-1/2 left-1/2 w-max active:border-2 active:border-dashed active:border-green-600`}
+                >
+                  <div className="w-full">
+                    <p className="text-4xl font-semibold text-center font-serif">
+                      {" "}
+                      CERTIFICATE{" "}
+                    </p>
+                    <div className="flex justify-center items-center  w-full">
+                      <input
+                        defaultValue={typeSertifikat}
+                        onChange={(e) => setTypeSertifikat(e.target.value)}
+                        className="border-0 bg-white/0 text-blue-gray  text-center w-full"
+                      />
+                    </div>
+                    <p className="text-semibold text-center  text-lg font-semibold m-4">
+                      Diberikan Kepada :
+                    </p>
+                  </div>
+                  <div className="">
+                    <p className="text-3xl font-semibold text-center font-serif mb-5">
+                      {e}
+                    </p>
+                    <p className="text-md font-semibold text-center font-serif w-[300px]  break-words">
+                      {perihal}
+                    </p>
+                  </div>
                 </div>
               </Draggable>
 
-              <Draggable position={defaultPositionKepsek} onStop={(e, data) => setDefaultPositionKepsek({ x: data.x, y: data.y })}>
-                <div className="text-xs w-full">
+              <Draggable
+                bounds="parent"
+                position={defaultPositionKepsek}
+                onStop={(e, data) =>
+                  setDefaultPositionKepsek({ x: data.x, y: data.y })
+                }
+              >
+                <div className="text-xs w-max absolute bottom-0 right-0 active:border-2 active:border-dashed active:border-green-600">
                   <p className="text-black font-bold">
                     {kepsek.data.data.user.username}
                   </p>
                   <p>Kepala Sekolah</p>
                 </div>
               </Draggable>
+              <Draggable
+                bounds="parent"
+                position={defaultPositionNomor}
+                onStop={(e, data) =>
+                  setDefaultPositionNomor({ x: data.x, y: data.y })
+                }
+              >
+                <div className="text-xs w-max absolute top-0 right-0 active:border-2 active:border-dashed active:border-green-600">
+                  <p className="text-black font-bold">{nomorSertifikat}</p>
+                </div>
+              </Draggable>
               {leadEvent && (
-                 <Draggable position={defaultPositionKepel} onStop={(e, data) => setDefaultPositionKepel({ x: data.x, y: data.y })}>
-                  <div className="text-xs w-full">
+                <Draggable
+                  bounds="parent"
+                  position={defaultPositionKepel}
+                  onStop={(e, data) =>
+                    setDefaultPositionKepel({ x: data.x, y: data.y })
+                  }
+                >
+                  <div className="text-xs w-max absolute bottom-0">
                     <p className="text-black font-bold">{leadEvent}</p>
                     <p>Ketua Pelaksana</p>
                   </div>
                 </Draggable>
               )}
               {isQrCode && (
-               <Draggable position={defaultPositionQrCode} onStop={(e, data) => setDefaultPositionQrCode({ x: data.x, y: data.y })}>
-                  <QrCode value={"Testing 123"} size={qrCodeSize} />
+                <Draggable
+                  bounds="parent"
+                  position={defaultPositionQrCode}
+                  onStop={(e, data) =>
+                    setDefaultPositionQrCode({ x: data.x, y: data.y })
+                  }
+                >
+                  <div className="absolute top-1/2 left-1/2 w-max active:border-2 active:border-dashed active:border-green-600">
+                    <QrCode
+                      value={`SERTIFIKAT INI DI KELUARKAN OLEH SMAN 1 KARANG INTAN UNTUK  ${e} atas ${perihal} dan sertifikat ini di keluarkan pada ${new Date().toLocaleString()}`}
+                      size={qrCodeSize}
+                    />
+                  </div>
                 </Draggable>
               )}
-            </>
-          </div>
-        ))}
+            </div>
+          ))}
       </div>
 
-      <ButtonCustom text={"Cetak"} onClick={handlePrint} />
+      <ButtonCustom
+        text={
+          <div className="flex gap-2 justify-center items-center ">
+            Download dan Print {loading && <Loader  />}
+          </div>
+        }
+        disabled={loading}
+        onClick={() => generatePDF()}
+      />
     </div>
   );
 }

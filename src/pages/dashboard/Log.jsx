04e9eb9062e3b2d8c3759085f91ed394203950b1
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-11-26 03:02:03 +0000

    feat:certificate done

--- src/pages/dashboard/Log.jsx
+++ src/pages/dashboard/Log.jsx
@@ -78,7 +78,7 @@ export default function Log() {
                   disabled={handleDeleteAllLog.isLoading}
                   onClick={handleDeleteAllLog.mutate}
                 />
-              </div>,
+              </div>
             )}
         </div>
       </CardHeader>

ff4803a9ea4fb5567166127efcd95a86ab7ccbe8
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-12-12 10:44:00 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        worker: "./src/services/worker.js",
-      },
-    },
-  },
 });

af10b021b25a2fe041b89241c7a0d294e1677ca2
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-12-12 12:16:36 +0000

    Update 3 files

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import ViteWorkerPlugin from "worker-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteWorkerPlugin()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
   worker: {
+    // Konfigurasi tambahan untuk worker jika diperlukan
+    // Misalnya, Anda dapat menentukan format atau menambahkan opsi lainnya.
+    // Lihat dokumentasi worker-plugin untuk opsi-opsi yang tersedia:
+    // https://github.com/GoogleChromeLabs/worker-plugin#options
     format: "es",
   },
   build: {
     rollupOptions: {
       output: {
-        format: "module",
+        format: "esm",
       },
     },
   },

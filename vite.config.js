dbdb26f3e567102c85bc5b2a6d67fc10666cfa4e
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-12-12 09:25:17 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  formats: ["es"],
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],

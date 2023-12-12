ef4676bb2c382bbc10cca0ed392ffea2f6169bc8
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-12-12 11:26:23 +0000

    Update worker.js

--- src/services/worker.js
+++ src/services/worker.js
@@ -12,31 +12,32 @@ onmessage = (data) => {
   const { screenshots, perihal } = data.data;
 
   // Fungsi generatePDF
-  async function generatePDF() {
-    console.log("worker running");
-    const pdf = new jsPDF("l", "mm", "a4");
 
-    screenshots?.forEach((screenshot, i) => {
-      if (i > 0) {
-        pdf.addPage();
-      }
-      pdf.addImage(
-        screenshot.data,
-        "JPEG",
-        0,
-        0,
-        pdf.internal.pageSize.width,
-        pdf.internal.pageSize.height,
-      );
-    });
-
-    pdf.autoPrint();
-    const pdfName = `${perihal}-${new Date().toLocaleString()}.pdf`;
-    pdf.save(pdfName);
-    const pdfData = pdf.output("blob");
-
-    postMessage({ pdfName, pdfData });
-  }
-
-  generatePDF();
+  generatePDF(screenshots, perihal);
 };
+
+async function generatePDF(screenshots, perihal) {
+  console.log("worker running");
+  const pdf = new jsPDF("l", "mm", "a4");
+
+  screenshots?.forEach((screenshot, i) => {
+    if (i > 0) {
+      pdf.addPage();
+    }
+    pdf.addImage(
+      screenshot.data,
+      "JPEG",
+      0,
+      0,
+      pdf.internal.pageSize.width,
+      pdf.internal.pageSize.height,
+    );
+  });
+
+  pdf.autoPrint();
+  const pdfName = `${perihal}-${new Date().toLocaleString()}.pdf`;
+  pdf.save(pdfName);
+  const pdfData = pdf.output("blob");
+
+  postMessage({ pdfName, pdfData });
+}

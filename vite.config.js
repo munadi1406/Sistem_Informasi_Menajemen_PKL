3ec417ba30c2eaaacb437b9b74e6d622595937b3
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-12-12 22:14:05 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import ViteWorkerPlugin from "worker-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +8,6 @@ export default defineConfig({
     alias: [{ find: "@", replacement: "/src" }],
   },
   worker: {
-    plugins: [new ViteWorkerPlugin()],
     format: "es",
   },
   build: {

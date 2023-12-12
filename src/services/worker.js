0b0a4a63a84733f9410dec8f4f3acd6fa76da06d
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-12-12 11:09:43 +0000

    Update worker.js

--- src/services/worker.js
+++ src/services/worker.js
@@ -7,41 +7,36 @@ import jsPDF from "jspdf";
 // import html2canvas from "../../node_modules/html2canvas/dist/html2canvas.js";
 
 // Menanggapi pesan dari utama
-onmessage = (event) => {
-  const { screenshots, splitName, perihal } = event.data || {};
+onmessage = (data) => {
+  console.log(data);
+  const { screenshots, perihal } = data.data;
 
-  // Pastikan bahwa screenshots adalah array sebelum menggunakan forEach
-  if (Array.isArray(screenshots)) {
-    // Fungsi generatePDF ditempatkan di luar blok onmessage
-    generatePDF(screenshots, splitName, perihal);
-  } else {
-    console.error("Screenshots data is missing or not an array.");
+  // Fungsi generatePDF
+  async function generatePDF() {
+    console.log("worker running");
+    const pdf = new jsPDF("l", "mm", "a4");
+    let image = screenshots ?? [];
+    image.forEach((screenshot, i) => {
+      if (i > 0) {
+        pdf.addPage();
+      }
+      pdf.addImage(
+        screenshot.data,
+        "JPEG",
+        0,
+        0,
+        pdf.internal.pageSize.width,
+        pdf.internal.pageSize.height,
+      );
+    });
+
+    pdf.autoPrint();
+    const pdfName = `${perihal}-${new Date().toLocaleString()}.pdf`;
+    pdf.save(pdfName);
+    const pdfData = pdf.output("blob");
+
+    postMessage({ pdfName, pdfData });
   }
+
+  generatePDF();
 };
-
-// Fungsi generatePDF ditempatkan di luar blok onmessage
-async function generatePDF(screenshots, splitName, perihal) {
-  console.log("worker running");
-  const pdf = new jsPDF("l", "mm", "a4");
-
-  screenshots.forEach((screenshot, i) => {
-    if (i > 0) {
-      pdf.addPage();
-    }
-    pdf.addImage(
-      screenshot.data,
-      "JPEG",
-      0,
-      0,
-      pdf.internal.pageSize.width,
-      pdf.internal.pageSize.height,
-    );
-  });
-
-  pdf.autoPrint();
-  const pdfName = `${perihal}-${new Date().toLocaleString()}.pdf`;
-  pdf.save(pdfName);
-  const pdfData = pdf.output("blob");
-
-  postMessage({ pdfName, pdfData });
-}

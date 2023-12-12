f210b08c536cdafa1b64e0305027fbade3c4df03
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-12-12 11:32:25 +0000

    Update worker.js

--- src/services/worker.js
+++ src/services/worker.js
@@ -9,36 +9,34 @@ import jsPDF from "jspdf";
 // Menanggapi pesan dari utama
 onmessage = (data) => {
   console.log(data);
-  const { screenshots, perihal } = data.data;
 
   // Fungsi generatePDF
+  async function generatePDF() {
+    console.log("worker running");
+    const { screenshots, perihal } = data.data;
+    const pdf = new jsPDF("l", "mm", "a4");
+    let image = screenshots || [];
+    image.forEach((screenshot, i) => {
+      if (i > 0) {
+        pdf.addPage();
+      }
+      pdf.addImage(
+        screenshot.data,
+        "JPEG",
+        0,
+        0,
+        pdf.internal.pageSize.width,
+        pdf.internal.pageSize.height,
+      );
+    });
 
-  generatePDF(screenshots, perihal);
+    pdf.autoPrint();
+    const pdfName = `${perihal}-${new Date().toLocaleString()}.pdf`;
+    pdf.save(pdfName);
+    const pdfData = pdf.output("blob");
+
+    postMessage({ pdfName, pdfData });
+  }
+
+  generatePDF();
 };
-
-async function generatePDF(screenshots, perihal) {
-  console.log("worker running");
-  const pdf = new jsPDF("l", "mm", "a4");
-
-  // screenshots?.forEach((screenshot, i) => {
-  //   if (i > 0) {
-  //     pdf.addPage();
-  //   }
-  //   pdf.addImage(
-  //     screenshot.data,
-  //     "JPEG",
-  //     0,
-  //     0,
-  //     pdf.internal.pageSize.width,
-  //     pdf.internal.pageSize.height,
-  //   );
-  // });
-
-  // pdf.autoPrint();
-  // const pdfName = `${perihal}-${new Date().toLocaleString()}.pdf`;
-  // pdf.save(pdfName);
-  // const pdfData = pdf.output("blob");
-
-  // postMessage({ pdfName, pdfData });
-  postMessage("boo");
-}

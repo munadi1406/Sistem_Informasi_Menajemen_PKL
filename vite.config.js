ef3be14475fed2e33b7dfcc86e5858496ea5d1c0
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-12-12 09:24:07 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  formats: ["es"],
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],

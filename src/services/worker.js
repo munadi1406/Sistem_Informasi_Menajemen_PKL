e9fb83649966461a319886cf44ec867bd4a7d5bd
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-12-12 11:28:04 +0000

    Update worker.js

--- src/services/worker.js
+++ src/services/worker.js
@@ -20,24 +20,25 @@ async function generatePDF(screenshots, perihal) {
   console.log("worker running");
   const pdf = new jsPDF("l", "mm", "a4");
 
-  screenshots?.forEach((screenshot, i) => {
-    if (i > 0) {
-      pdf.addPage();
-    }
-    pdf.addImage(
-      screenshot.data,
-      "JPEG",
-      0,
-      0,
-      pdf.internal.pageSize.width,
-      pdf.internal.pageSize.height,
-    );
-  });
+  // screenshots?.forEach((screenshot, i) => {
+  //   if (i > 0) {
+  //     pdf.addPage();
+  //   }
+  //   pdf.addImage(
+  //     screenshot.data,
+  //     "JPEG",
+  //     0,
+  //     0,
+  //     pdf.internal.pageSize.width,
+  //     pdf.internal.pageSize.height,
+  //   );
+  // });
 
-  pdf.autoPrint();
-  const pdfName = `${perihal}-${new Date().toLocaleString()}.pdf`;
-  pdf.save(pdfName);
-  const pdfData = pdf.output("blob");
+  // pdf.autoPrint();
+  // const pdfName = `${perihal}-${new Date().toLocaleString()}.pdf`;
+  // pdf.save(pdfName);
+  // const pdfData = pdf.output("blob");
 
-  postMessage({ pdfName, pdfData });
+  // postMessage({ pdfName, pdfData });
+  postMessage("boo");
 }
